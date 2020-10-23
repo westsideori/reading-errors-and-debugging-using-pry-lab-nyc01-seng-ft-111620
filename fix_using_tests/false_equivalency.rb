397248642340c0a1fc729111814b817373ca27f1
inventive-view-7245 <ori.markowitz@gmail.com>
2020-10-23 11:07:53 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,9 +12,9 @@ end
 
 def selection(num)
   if num == 1
-     print "YUM YUM MUNCH MUNCH MUNCH"
+     p "YUM YUM MUNCH MUNCH MUNCH"
   elsif num == 2
-     print "HAM HAM HAM IN MY TUMMY"
+     p "HAM HAM HAM IN MY TUMMY"
   end
 end
 
